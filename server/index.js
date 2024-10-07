ff43b7cbfd7a485f935e958c4daf851391c3a28f
akshitshukrawal <akshitshukrawal@gmail.com>
2024-10-07 20:27:11 +0530

    Updated index.js and vercel.json for deployment

--- server/index.js
+++ server/index.js
@@ -38,17 +38,13 @@ mongoose.connect(mongoURI, {
 });
 
 // Routes
-const front = (req, res) => {
+app.get('/', (req, res) => {
     res.json("Hello, World!");
-};
+});
 
-app.use('/', front);
 app.use('/api/users', userRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/events', eventRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Export the app for Vercel
+export default app;
